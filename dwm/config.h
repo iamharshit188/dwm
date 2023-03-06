7c308d42e6f1390fda17a7fd6f0e98975030f76f
Harshit Tiwari <iamharshit188@gmail.com>
2023-03-07 00:08:14 +0530

    Revert back to simple terminal in dwm , added scrollback support in Simple Terminal

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=10:antialias=true:autohint:true" };
+static const char *fonts[]          = { "JetBrains Mono:size=10:antialias=true:autohint:true" , "FiraCode Nerd Font:size=10" , "3270 Nerd Font:size=10" };
 static const char dmenufont[]       = "JetBrains:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
