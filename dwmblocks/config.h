76502924640d3a07993e3fd4ad9abbf804afeeea
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-03-25 00:27:06 +0530

    patched dwmblocks to float and render backgroung image above it

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -2,11 +2,10 @@
 static const Block blocks[] = {
 
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"",  "cpu",  1,  16},
   {"",  "nettraf",  1,  16},
   {"",  "memory", 1,  10},
   {"",  "battery",  1, 1},
-  {"",  "clock", 5,  4},
+  {"",  "cpu", 5,  4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
