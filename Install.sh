36ab94aa572bee0bce75960079e8f921ee513555
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-07-22 09:42:44 +0530

    Updated Script , Now working :)

--- Install.sh
+++ Install.sh
@@ -1,4 +1,8 @@
-
+## Installing DWM dependencies
+sudo pacman -S libx11 libxft imagemagick feh libxinerama xorg-server xorg-xinit ttf-jetbrains-mono noto-fonts python-pip --noconfirm 
+## Installing Pywal for Wallpaper color support
+pip install pywal 
+## Installing DWM
 cd dwm
 echo "Installing DWM :) and applying patches"
 sudo make clean install
@@ -15,5 +19,13 @@ sudo make clean install
 echo "Installing slock lockscreen, Unpatched due to Security issues:)"
 cd ../slock
 sudo make clean install
-
+## Adding all the fonts
+cd ../
+sudo mv Fonts/* /usr/share/fonts/
+mkdir -p ~/.local/bin/
+mv ../Scripts/* ~/.local/bin/
+cd ..
+mv .xinit .bashrc ~/
+## Adding touchpad support for laptops
+mv ./30-touchpad.conf /etc/X11/xorg.conf.d/
 
