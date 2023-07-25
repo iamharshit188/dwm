08891235dfa2cc9998047bb9f76ccc67e3a6c84d
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2023-07-25 12:51:20 +0530

    Updated Install Script

--- Install.sh
+++ Install.sh
@@ -1,31 +1,36 @@
 ## Installing DWM dependencies
 sudo pacman -S libx11 libxft imagemagick feh libxinerama xorg-server xorg-xinit ttf-jetbrains-mono noto-fonts python-pip trash-cli --noconfirm 
 ## Installing Pywal for Wallpaper color support
-pip install pywal 
+pip install pywal --break
 ## Installing DWM
 cd dwm
 echo "Installing DWM :) and applying patches"
 sudo make clean install
+echo "DWM Installed"
 cd ../st
-echo "Installing st just in case alacritty isnt supported by your GPU ;)"
+echo "Installing st (Simple Terminal) ;)"
 sudo make clean install 
-echo "Patches applied"
+echo "Simple Terminal Installed"
 cd ../dmenu
 echo "Installing dmenu :)"
 sudo make clean install
+echo "dmenu installed"
 cd ../dwmblocks
 echo "Installing dwmblocks and applying patches :)"
 sudo make clean install
-echo "Installing slock lockscreen, Unpatched due to Security issues:)"
-cd ../slock
-sudo make clean install
-## Adding all the fonts
-cd ../
-sudo mv Fonts/* /usr/share/fonts/
+echo "DWMblocks Installed"
+## Adding all the Fonts
+cd ./Fonts/
+sudo mv * /usr/share/fonts/
 mkdir -p ~/.local/bin/
-mv ../Scripts/* ~/.local/bin/
+cd .././Scripts/  
+mv * ~/.local/bin/
 cd ..
-mv .xinit .bashrc ~/
+mv ./.xinit ./.bashrc ~/
 ## Adding touchpad support for laptops
-mv ./30-touchpad.conf /etc/X11/xorg.conf.d/
+sudo mv ./30-touchpad.conf /etc/X11/xorg.conf.d/
+clear
+echo "DWM has been installed , Simple Terminal has been installed , now reboot "
+echo "type startx after logging in to tty"
+echo "Thank you for using my dots ;)"
 
