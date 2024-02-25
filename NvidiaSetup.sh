0a8f10228ab95edfcc041d8b8bf3245cf54f21e1
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2024-02-25 12:45:27 +0530

    Added nvidiaSetup.sh " Not Optimised

---
+++ NvidiaSetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "A minimal Script to Install Driver's for Nvidia Graphic cards"
+# Installing Necessary Packages.
+sudo pacman -S nvidia nvidia-utils opencl-nvidia bumblebee glxinfo
+
+
