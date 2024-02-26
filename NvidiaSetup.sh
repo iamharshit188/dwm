567c23a4ea81968a475713d68d175307d2e0651d
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2024-02-26 19:23:36 +0530

    Added Scripts to ~/.local/bin

--- NvidiaSetup.sh
+++ NvidiaSetup.sh
@@ -3,5 +3,6 @@
 echo "A minimal Script to Install Driver's for Nvidia Graphic cards"
 # Installing Necessary Packages.
 sudo pacman -S nvidia nvidia-utils opencl-nvidia bumblebee glxinfo
+mv ./NvidiaScripts/* ~/.local/bin/
 
 
